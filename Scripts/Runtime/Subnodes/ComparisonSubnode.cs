258c9e7c8a1b79deeaf216bf596c4eff7dfe4504
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2023-01-03 20:52:24 -0500

    Update ComparisonSubnode.cs

--- Scripts/Runtime/Subnodes/ComparisonSubnode.cs
+++ Scripts/Runtime/Subnodes/ComparisonSubnode.cs
@@ -8,7 +8,7 @@ namespace MPewsey.BehaviorTree.Subnodes
     public abstract class ComparisonSubnode<T> : BehaviorSubnode
     {
         [SerializeField]
-        private string _key = "<None>";
+        private string _key;
         /// <summary>
         /// The blackboard key.
         /// </summary>
